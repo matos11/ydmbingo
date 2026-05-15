dd8a53f928febde1445a06c444b9c1b239b1fd04
matos1121 <yisak1121@gmail.com>
2026-05-15 14:19:59 +0300

    updated multiplayer bingo

--- server.js
+++ server.js
@@ -1,38 +1,156 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+const io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
 });
 
-io.on("connection", (socket) => {
+let players = [];
 
-  console.log("Player connected");
+let drawnNumbers = [];
 
-  socket.on("draw-number", (number) => {
+let gameStarted = false;
 
-    io.emit("new-number", number);
+let gameInterval = null;
 
-  });
+function generateNumber(){
 
-  socket.on("disconnect", () => {
+    let number;
 
-    console.log("Disconnected");
+    do{
 
-  });
+        number =
+        Math.floor(Math.random()*75)+1;
+
+    }while(drawnNumbers.includes(number));
+
+    drawnNumbers.push(number);
+
+    return number;
+
+}
+
+function startCountdown(){
+
+    let sec = 10;
+
+    const countdown =
+    setInterval(()=>{
+
+        io.emit("countdown",sec);
+
+        sec--;
+
+        if(sec < 0){
+
+            clearInterval(countdown);
+
+            startGame();
+
+        }
+
+    },1000);
+
+}
+
+function startGame(){
+
+    gameStarted = true;
+
+    gameInterval =
+    setInterval(()=>{
+
+        if(drawnNumbers.length >= 75){
+
+            clearInterval(gameInterval);
+
+            return;
+
+        }
+
+        const number = generateNumber();
+
+        io.emit("new-number",number);
+
+    },4500);
+
+}
+
+function resetGame(){
+
+    clearInterval(gameInterval);
+
+    players = [];
+
+    drawnNumbers = [];
+
+    gameStarted = false;
+
+    io.emit("new-game");
+
+}
+
+io.on("connection",(socket)=>{
+
+    console.log("Player Connected");
+
+    socket.on("join-game",(player)=>{
+
+        players.push({
+
+            id:socket.id,
+            ...player
+
+        });
+
+        io.emit("player-count",players.length);
+
+        if(players.length >= 2 && !gameStarted){
+
+            startCountdown();
+
+        }
+
+    });
+
+    socket.on("claim-bingo",(data)=>{
+
+        if(!gameStarted) return;
+
+        clearInterval(gameInterval);
+
+        io.emit("winner",data);
+
+        setTimeout(()=>{
+
+            resetGame();
+
+        },8000);
+
+    });
+
+    socket.on("disconnect",()=>{
+
+        players =
+        players.filter(p=>p.id !== socket.id);
+
+        io.emit("player-count",players.length);
+
+        console.log("Disconnected");
+
+    });
 
 });
 
-server.listen(3000, () => {
-  console.log("Server started");
+server.listen(3000,()=>{
+
+    console.log("Server Running");
+
 });
